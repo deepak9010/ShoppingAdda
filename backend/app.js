6781e440459abe7326849f6d154fd6c547c03f11
deepak9010 <deepak23072019@gmail.com>
2025-05-04 13:22:51 +0530

    remove header

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,6 @@ app.use(cors({
   // origin : "*",
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json());
