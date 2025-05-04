e8f9c5cad6cab68c696f5a108f68531a1e20c70b
deepak9010 <deepak23072019@gmail.com>
2025-05-04 13:17:21 +0530

    add header in cors

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ app.use(cors({
   // origin : "*",
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json());
